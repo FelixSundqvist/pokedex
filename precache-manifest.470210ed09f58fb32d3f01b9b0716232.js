e1cc3abb25470675b9f3a093d80cfcc6cfc98282
FelixSundqvist <felix.sundqvist@outlook.com>
2019-06-25 17:33:08 +0300

    Updates

---
+++ precache-manifest.470210ed09f58fb32d3f01b9b0716232.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd2d4d4cb280e7683b7b0d2b40f1f37",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0196b243ac0da9908a38",
+    "url": "/pokedex/static/css/main.2107eda2.chunk.css"
+  },
+  {
+    "revision": "3a25c1ed1de3a3fecb3e",
+    "url": "/pokedex/static/js/2.fa3c4476.chunk.js"
+  },
+  {
+    "revision": "cfae7b752b7d1bd51b65",
+    "url": "/pokedex/static/js/3.9de514d4.chunk.js"
+  },
+  {
+    "revision": "0196b243ac0da9908a38",
+    "url": "/pokedex/static/js/main.56934838.chunk.js"
+  },
+  {
+    "revision": "fce8cbbb1a236e031562",
+    "url": "/pokedex/static/js/runtime~main.0ff6b9d2.js"
+  },
+  {
+    "revision": "3ada9815c06d2619d52d16a5601c96b2",
+    "url": "/pokedex/static/media/Early_GameBoy.3ada9815.ttf"
+  },
+  {
+    "revision": "e83936392eebb34d9a99c37987256636",
+    "url": "/pokedex/static/media/pokedex-lens.e8393639.svg"
+  }
+]);
