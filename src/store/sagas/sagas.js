d941226e019aa038dc701ab45baf513e8ff10682
FelixSundqvist <felix.sundqvist@outlook.com>
2019-07-05 16:47:37 +0300

    create axios instance w/ timeout

--- src/store/sagas/sagas.js
+++ src/store/sagas/sagas.js
@@ -2,12 +2,12 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import axios from 'axios';
 import generations from '../../PokemonGeneration';
+import axiosInstance from '../../axiosInstance';
 /* generations[actions.selectedGen].end */
 function* startFetchAll(actions){
     try{
         yield put({type: actionTypes.LOADING_POKEMONS })
-        const pokemons = yield call(() => axios
-            .get(`https://pokeapi.co/api/v2/pokemon/?limit=${generations[actions.selectedGen].end}&offset=${generations[actions.selectedGen].start}`)
+        const pokemons = yield call(() => axiosInstance(`/pokemon/?limit=${generations[actions.selectedGen].end}&offset=${generations[actions.selectedGen].start}`)
             .then(res => res.data)
             .then(data => data.results)
        );
@@ -25,7 +25,7 @@ function* startFetchSelected(actions){
         yield put({ type: actionTypes.LOADING_CURRENT_PKMN })
 
         const selectedPokemon = yield call(
-            ()=> axios.get('https://pokeapi.co/api/v2/pokemon/'+actions.id)
+            ()=> axiosInstance('https://pokeapi.co/api/v2/pokemon/'+actions.id)
                 .then(res => res.data)
                 .then(data => data)    
         );
@@ -33,7 +33,7 @@ function* startFetchSelected(actions){
         const species = actions.id.replace(/(-[a-z]{3,})?(-x|-y)?$/,"");
   
         const pokedexInfo = yield call(
-            ()=> axios.get('https://pokeapi.co/api/v2/pokemon-species/'+species)
+            ()=> axiosInstance('https://pokeapi.co/api/v2/pokemon-species/'+species)
                 .then(res => res.data)
                 .then(data => data)); 
         
